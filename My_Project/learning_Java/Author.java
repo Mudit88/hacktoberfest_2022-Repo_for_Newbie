512dd81854215af19c95e641eb7889f8abf2f80c
Jaival <jaival.s.vyas@gmail.com>
2022-10-01 20:44:00 +0530

    created getter-setter for both class

--- My_Project/learning_Java/Author.java
+++ My_Project/learning_Java/Author.java
@@ -16,4 +16,20 @@ public class Author {
         this.author_name = author_name;
         this.author_pen_name = author_pen_name;
     }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getAuthor_pen_name() {
+        return author_pen_name;
+    }
+
+    public void setAuthor_pen_name(String author_pen_name) {
+        this.author_pen_name = author_pen_name;
+    }
 }
--- My_Project/learning_Java/Book.java
+++ My_Project/learning_Java/Book.java
@@ -23,4 +23,44 @@ public class Book {
         this.price = price;
         this.author = author;
     }
+
+    public String getISBN_No() {
+        return ISBN_No;
+    }
+
+    public void setISBN_No(String ISBN_No) {
+        this.ISBN_No = ISBN_No;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
